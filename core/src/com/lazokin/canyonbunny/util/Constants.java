5ddf1f73200cf6c2dde23c78aa63922876343ce8
Nik Ambukovski <lazokin@hotmail.com>
2016-12-17 15:16:03 +1100

    handling level data

--- core/src/com/lazokin/canyonbunny/util/Constants.java
+++ core/src/com/lazokin/canyonbunny/util/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public static final float VIEWPORT_WIDTH = 5.0f;
     public static final float VIEWPORT_HEIGHT = 5.0f;
     public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack.atlas";
+    public static final String LEVEL_01 = "levels/level-01.png";
 
 }
