16a208599f5e541a8eeddd9d7da2ec5c029d1cf9
Nik Ambukovski <lazokin@hotmail.com>
2016-12-17 14:32:51 +1100

    creating the texture atlas

--- desktop/src/com/lazokin/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/lazokin/canyonbunny/desktop/DesktopLauncher.java
@@ -2,13 +2,30 @@ package com.lazokin.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.lazokin.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
+
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+
 	public static void main (String[] arg) {
+
+		if (rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images", "../android/assets/images", "canyonbunny.pack");
+		}
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new CanyonBunnyMain(), config);
+
 	}
 }
