38d97b0500e8e53992f4c4edc1257e1ad79ade82
Nik Ambukovski <lazokin@hotmail.com>
2017-02-20 22:22:53 +1100

    creating game objects

--- core/src/com/lazokin/canyonbunny/game/WorldController.java
+++ core/src/com/lazokin/canyonbunny/game/WorldController.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
